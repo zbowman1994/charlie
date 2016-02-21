c5eb2527cae853c58f809417cdd2be4f275af74f
rossgb <r0554613@gmail.com>
2016-02-20 17:15:18 -0800

    dont go for danger food

--- app/main.py
+++ app/main.py
@@ -169,6 +169,14 @@ def move():
         path_length = len(tentative_path)
         snek_length = len(snek_coords) + 1
 
+        dead = False
+        for enemy in data['snakes']:
+            if enemy['id'] == ID:
+                continue
+            if path_length > distance(enemy['coords'][0], food):
+                dead = True
+        if dead:
+            continue
 
         # Update snek
         if path_length < snek_length:
